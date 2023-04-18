f3f4cea9cf2dc1fe3c82c05f5f62e7fa5e98a1f8
Nicolas Lima <nicolasaraujo845@gmail.com>
2023-04-18 10:34:44 -0300

    autenticação signup

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -37,6 +37,29 @@ export const AuthProvider = ({ children }) => {
         } else {
             return "Usuário não cadastrado"
         }
+    };
+
+    const signup = (email, password) => {
+        const userStorage = JSON.parse(localStorage.getItem("users_db"));
+
+        const hasUser = usersStorage?.filter((user) => user.email === email)
+
+        if (hasUser?.length) {
+            return "Já tem uma conta com esse E-mail";
+        }
+
+        let newUser;
+
+        if (usersStorage) {
+            newUser = [...userStorage, { email, password }];
+        } else {
+            newUser = [{ email, password }]
+        }
+
+        localStorage.setItem("users_db", JSON.stringify(newUser));
+
+        return;
     }
+
     return <AuthContext.Provider>{children}</AuthContext.Provider>
 }
