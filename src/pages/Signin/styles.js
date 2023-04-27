0e6311a9581ca62e75523da2e85c0a3b499acab9
Nicolas Lima <nicolasaraujo845@gmail.com>
2023-04-27 10:25:22 -0300

    Signin lógica

--- src/pages/Signin/styles.js
+++ src/pages/Signin/styles.js
@@ -33,3 +33,16 @@ export const LabelSignup = styled.label`
     font-size: 16px;
     color: #676767;
 `;
+
+export const LabelError = styled.label`
+    font-size: 14px;
+    color: red;
+`;
+
+export const Strong = styled.strong`
+    cursor: pointer;
+    a{
+        text-decoration: none;
+        color: #676767;
+    }    
+`;
